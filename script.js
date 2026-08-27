2a3a0c9f4a538bb5ed5ca631ac6c3fced2942348
fidelvti <fidelvti@gmail.com>
2026-08-27 22:17:44 +0200

    Bump watchface cache version

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function imageTiles(folder, prefix, count) {
   }));
 }
 
-const watchfaceVersion = "20260827-nippon-even-1";
+const watchfaceVersion = "20260827-ocean-carbon-1";
 
 const collections = [
   {
